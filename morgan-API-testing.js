94fa5be935211f2b652d220210b651ede028e449
Morgan <morganw118@gmail.com>
2020-11-12 11:32:08 -0800

    minor changes to morganjs

--- morgan-API-testing.js
+++ morgan-API-testing.js
@@ -1,59 +1,85 @@
 // MORGAN'S CODE, PLS DONT TOUCH
 // var apiKey = 391053-Musicolo-DLE4BMNM
 
-// var similarArray = [0, 1, 2];
+var similarArray = [0, 1, 2];
 
-// $(document).on('keypress',function(e) {
-//     if(e.which == 13) {
+// Key down event for 'return' key
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
         
-//         var artist = encodeURIComponent($('.search').val().toLowerCase());
-//         var tasteDive = 'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=' + artist + '&type=music&k=391053-Musicolo-DLE4BMNM';
+        var artist = encodeURIComponent($('.search').val().toLowerCase());
+        var tasteDive = 'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=' + artist + '&type=music&k=391053-Musicolo-DLE4BMNM';
         
-//         var urlArray = [];
-//         // var imgArray = [];
-
-//         $.ajax({
-//             url: tasteDive,
-//             method: 'GET'
-//         }).then(function(response) {
-//             console.log(response);
-//             for (elem of similarArray) {
-//                 var simArtist = response.Similar.Results[elem].Name;
-//                 var domElem = $('.card-title-'+ elem);
-//                 domElem.text(simArtist);
-
-//                 var simArtist = encodeURIComponent(simArtist);
-//                 var simImgURL = "https://rest.bandsintown.com/artists/" + simArtist + "?app_id=codingbootcamp";
-
-//                 urlArray.push(simImgURL);
-//             }
-
-//             $.ajax({
-//                 url: urlArray[0],
-//                 method: "GET",
-//             }).then(function(res) {
-//                 $('#sim-artist-img-0').attr('src', res.image_url);
-//             });
-
-//             $.ajax({
-//                 url: urlArray[1],
-//                 method: "GET",
-//             }).then(function(res) {
-//                 $('#sim-artist-img-1').attr('src', res.image_url);
-//             });
-
-//             $.ajax({
-//                 url: urlArray[2],
-//                 method: "GET",
-//             }).then(function(res) {
-//                 $('#sim-artist-img-2').attr('src', res.image_url);
-//             });
-//         });
-//     }
-// });
+        var urlArray = [];
+        // var imgArray = [];
 
 
+        // Ajax call to 'taste-dive' API to get names of similar artists
+        $.ajax({
+            url: tasteDive,
+            method: 'GET'
+        }).then(function(response) {
+            console.log(response);
+            // Pasting the similar artists name to each dom element
+            for (elem of similarArray) {
+                var simArtist = response.Similar.Results[elem].Name;
+                var domElem = $('.card-title-'+ elem);
+                domElem.text(simArtist);
 
+                var simArtist = encodeURIComponent(simArtist);
+                var simImgURL = "https://rest.bandsintown.com/artists/" + simArtist + "?app_id=codingbootcamp";
+
+                urlArray.push(simImgURL);
+            }
+            //  Ajax call to 'Bandsintown' API for the photos of similar artists 
+            $.ajax({
+                url: urlArray[0],
+                method: "GET",
+            }).then(function(res) {
+                $('#sim-artist-img-0').attr('src', res.image_url);
+            });
+
+            $.ajax({
+                url: urlArray[1],
+                method: "GET",
+            }).then(function(res) {
+                $('#sim-artist-img-1').attr('src', res.image_url);
+            });
+
+            $.ajax({
+                url: urlArray[2],
+                method: "GET",
+            }).then(function(res) {
+                $('#sim-artist-img-2').attr('src', res.image_url);
+            });
+            
+        });
+    }
+});
+
+
+            // ------------- CODE THAT WE TRIED ----------------
+            // var simImg= [];
+            // var domImg = [];
+            // var fetchImg = [];
+            // for (i=0; i < urlArray.length; i++) {
+            //     fetchImg = urlArray[i];
+            //     $.ajax({
+            //         url: fetchImg[i],
+            //         method: "GET"
+            //     }).then(function(res) {
+            //         console.log(res.image_url);
+            //         simImg[i] = res.image_url;
+            //         domImg[i] = $('#sim-artist-img-'+ urlArray.indexOf(i));
+            //         console.log(domImg[i]);
+            //         domImg[i].attr('src', simImg[i]);
+            //         imgArray.push(simImg[i]);
+            //         console.log(imgArray);
+            //     });
+            // }
+
+
+            // ----------------- CODE THAT I ORIGINALLY TRIED --------------------
             // for (item of urlArray) {
             //     var fetchImg = item;
             //     $.ajax({
