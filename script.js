dec2265d712130810a66d62cd71161314071ecfe
Morgan <morganw118@gmail.com>
2020-11-12 11:46:05 -0800

    merged my html and js files into the main

--- script.js
+++ script.js
@@ -5,25 +5,56 @@
 
 var similarArray = [0, 1, 2];
 
+// Key down event for 'return' key
 $(document).on('keypress',function(e) {
     if(e.which == 13) {
         
-        var artist = encodeURIComponent($('#artist-input').val().toLowerCase());
+        var artist = encodeURIComponent($('.search').val().toLowerCase());
         var tasteDive = 'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=' + artist + '&type=music&k=391053-Musicolo-DLE4BMNM';
         
+        var urlArray = [];
+        // var imgArray = [];
+
+        // Ajax call to 'taste-dive' API to get names of similar artists
         $.ajax({
             url: tasteDive,
             method: 'GET'
         }).then(function(response) {
             console.log(response);
+            // Pasting the similar artists name to each dom element
             for (elem of similarArray) {
                 var simArtist = response.Similar.Results[elem].Name;
                 var domElem = $('.card-title-'+ elem);
                 domElem.text(simArtist);
+
+                var simArtist = encodeURIComponent(simArtist);
+                var simImgURL = "https://rest.bandsintown.com/artists/" + simArtist + "?app_id=codingbootcamp";
+
+                urlArray.push(simImgURL);
             }
+            //  Ajax call to 'Bandsintown' API for the photos of similar artists 
+            $.ajax({
+                url: urlArray[0],
+                method: "GET",
+            }).then(function(res) {
+                $('#sim-artist-img-0').attr('src', res.image_url);
+            });
+
+            $.ajax({
+                url: urlArray[1],
+                method: "GET",
+            }).then(function(res) {
+                $('#sim-artist-img-1').attr('src', res.image_url);
+            });
+
+            $.ajax({
+                url: urlArray[2],
+                method: "GET",
+            }).then(function(res) {
+                $('#sim-artist-img-2').attr('src', res.image_url);
+            });
+            
         });
-
-
     }
 });
 
