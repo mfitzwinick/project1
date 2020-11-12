87ebb3d1bec81b0c11fda04d6425d5d148c53ca4
Morgan <morganw118@gmail.com>
2020-11-11 16:04:36 -0800

    ajax call runs on keydown event

--- morgan-taste-dive.js
+++ morgan-taste-dive.js
@@ -1,18 +1,25 @@
-
+// MORGAN'S CODE, PLS DONT TOUCH
 // var apiKey = 391053-Musicolo-DLE4BMNM
-var tasteDive = 'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=kaskade&type=music&k=391053-Musicolo-DLE4BMNM'
+var tasteDive = 'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=drake&type=music&k=391053-Musicolo-DLE4BMNM'
 
 var similarArray = [0, 1, 2];
 
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        
+        
+        $.ajax({
+            url: tasteDive,
+            method: 'GET'
+        }).then(function(response) {
+            console.log(response);
+            for (elem of similarArray) {
+                var simArtist = response.Similar.Results[elem].Name;
+                var domElem = $('.card-title-'+ elem);
+                domElem.text(simArtist);
+            }
+        });
 
-$.ajax({
-    url: tasteDive,
-    method: 'GET'
-}).then(function(response) {
-    console.log(response);
-    for (elem of similarArray) {
-        var simArtist = response.Similar.Results[elem].Name;
-        var domElem = $('.card-title-'+ elem);
-        domElem.text(simArtist);
-        }
-    });
+
+    }
+});
