71aea0d7215b4a64a61fd1c2cb6732368fd6be0b
Morgan <morganw118@gmail.com>
2020-11-12 10:24:17 -0800

    merging so you guys have my updated code

--- morgan-API-testing.js
+++ morgan-API-testing.js
@@ -1,56 +1,56 @@
 // MORGAN'S CODE, PLS DONT TOUCH
 // var apiKey = 391053-Musicolo-DLE4BMNM
 
-var similarArray = [0, 1, 2];
+// var similarArray = [0, 1, 2];
 
-$(document).on('keypress',function(e) {
-    if(e.which == 13) {
+// $(document).on('keypress',function(e) {
+//     if(e.which == 13) {
         
-        var artist = encodeURIComponent($('.search').val().toLowerCase());
-        var tasteDive = 'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=' + artist + '&type=music&k=391053-Musicolo-DLE4BMNM';
+//         var artist = encodeURIComponent($('.search').val().toLowerCase());
+//         var tasteDive = 'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=' + artist + '&type=music&k=391053-Musicolo-DLE4BMNM';
         
-        var urlArray = [];
-        // var imgArray = [];
+//         var urlArray = [];
+//         // var imgArray = [];
 
-        $.ajax({
-            url: tasteDive,
-            method: 'GET'
-        }).then(function(response) {
-            console.log(response);
-            for (elem of similarArray) {
-                var simArtist = response.Similar.Results[elem].Name;
-                var domElem = $('.card-title-'+ elem);
-                domElem.text(simArtist);
+//         $.ajax({
+//             url: tasteDive,
+//             method: 'GET'
+//         }).then(function(response) {
+//             console.log(response);
+//             for (elem of similarArray) {
+//                 var simArtist = response.Similar.Results[elem].Name;
+//                 var domElem = $('.card-title-'+ elem);
+//                 domElem.text(simArtist);
 
-                var simArtist = encodeURIComponent(simArtist);
-                var simImgURL = "https://rest.bandsintown.com/artists/" + simArtist + "?app_id=codingbootcamp";
+//                 var simArtist = encodeURIComponent(simArtist);
+//                 var simImgURL = "https://rest.bandsintown.com/artists/" + simArtist + "?app_id=codingbootcamp";
 
-                urlArray.push(simImgURL);
-            }
+//                 urlArray.push(simImgURL);
+//             }
 
-            $.ajax({
-                url: urlArray[0],
-                method: "GET",
-            }).then(function(res) {
-                $('#sim-artist-img-0').attr('src', res.image_url);
-            });
+//             $.ajax({
+//                 url: urlArray[0],
+//                 method: "GET",
+//             }).then(function(res) {
+//                 $('#sim-artist-img-0').attr('src', res.image_url);
+//             });
 
-            $.ajax({
-                url: urlArray[1],
-                method: "GET",
-            }).then(function(res) {
-                $('#sim-artist-img-1').attr('src', res.image_url);
-            });
+//             $.ajax({
+//                 url: urlArray[1],
+//                 method: "GET",
+//             }).then(function(res) {
+//                 $('#sim-artist-img-1').attr('src', res.image_url);
+//             });
 
-            $.ajax({
-                url: urlArray[2],
-                method: "GET",
-            }).then(function(res) {
-                $('#sim-artist-img-2').attr('src', res.image_url);
-            });
-        });
-    }
-});
+//             $.ajax({
+//                 url: urlArray[2],
+//                 method: "GET",
+//             }).then(function(res) {
+//                 $('#sim-artist-img-2').attr('src', res.image_url);
+//             });
+//         });
+//     }
+// });
 
 
 
@@ -84,20 +84,20 @@ $(document).on('keypress',function(e) {
 //             $('.card-img-top').attr('src', simImg);
 //         });
 
-// $(document).on('keypress',function(e) {
-//     if(e.which == 13) {
-//         var artist = encodeURIComponent($('.search').val().toLowerCase());
-//         var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
-//         $.ajax({
-//             url: queryURL,
-//             method: "GET"
-//         }).then(function(response) {
-//             console.log(response);
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        var artist = encodeURIComponent($('.search').val().toLowerCase());
+        var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
 
-//             $("#artist-name").text(response.name);
-//         });
-//     }
-// });
+            $("#artist-name").text(response.name);
+        });
+    }
+});
 
 
 
