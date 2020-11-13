dce38d819664570652dae3961534ecf102c40fb6
Morgan <morganw118@gmail.com>
2020-11-12 18:32:52 -0800

    messing with animation in css

--- morgan-API-testing.js
+++ morgan-API-testing.js
@@ -1,61 +1,61 @@
 // MORGAN'S CODE, PLS DONT TOUCH
 // var apiKey = 391053-Musicolo-DLE4BMNM
 
-// var similarArray = [0, 1, 2];
+var similarArray = [0, 1, 2];
 
-// // Key down event for 'return' key
-// $(document).on('keypress',function(e) {
-//     if(e.which == 13) {
+// Key down event for 'return' key
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
         
-//         var artist = encodeURIComponent($('.search').val().toLowerCase());
-//         var tasteDive = 'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=' + artist + '&type=music&k=391053-Musicolo-DLE4BMNM';
+        var artist = encodeURIComponent($('.search').val().toLowerCase());
+        var tasteDive = 'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=' + artist + '&type=music&k=391053-Musicolo-DLE4BMNM';
         
-//         var urlArray = [];
-//         // var imgArray = [];
+        var urlArray = [];
+        // var imgArray = [];
 
 
-//         // Ajax call to 'taste-dive' API to get names of similar artists
-//         $.ajax({
-//             url: tasteDive,
-//             method: 'GET'
-//         }).then(function(response) {
-//             console.log(response);
-//             // Pasting the similar artists name to each dom element
-//             for (elem of similarArray) {
-//                 var simArtist = response.Similar.Results[elem].Name;
-//                 var domElem = $('.card-title-'+ elem);
-//                 domElem.text(simArtist);
+        // Ajax call to 'taste-dive' API to get names of similar artists
+        $.ajax({
+            url: tasteDive,
+            method: 'GET'
+        }).then(function(response) {
+            console.log(response);
+            // Pasting the similar artists name to each dom element
+            for (elem of similarArray) {
+                var simArtist = response.Similar.Results[elem].Name;
+                var domElem = $('.card-title-'+ elem);
+                domElem.text(simArtist);
 
-//                 var simArtist = encodeURIComponent(simArtist.toLowerCase());
-//                 var simImgURL = "https://rest.bandsintown.com/artists/" + simArtist + "?app_id=codingbootcamp";
+                var simArtist = encodeURIComponent(simArtist.toLowerCase());
+                var simImgURL = "https://rest.bandsintown.com/artists/" + simArtist + "?app_id=codingbootcamp";
 
-//                 urlArray.push(simImgURL);
-//             }
-//             //  Ajax call to 'Bandsintown' API for the photos of similar artists 
-//             $.ajax({
-//                 url: urlArray[0],
-//                 method: "GET",
-//             }).then(function(res) {
-//                 $('#sim-artist-img-0').attr('src', res.image_url);
-//             });
+                urlArray.push(simImgURL);
+            }
+            //  Ajax call to 'Bandsintown' API for the photos of similar artists 
+            $.ajax({
+                url: urlArray[0],
+                method: "GET",
+            }).then(function(res) {
+                $('#sim-artist-img-0').attr('src', res.image_url);
+            });
 
-//             $.ajax({
-//                 url: urlArray[1],
-//                 method: "GET",
-//             }).then(function(res) {
-//                 $('#sim-artist-img-1').attr('src', res.image_url);
-//             });
+            $.ajax({
+                url: urlArray[1],
+                method: "GET",
+            }).then(function(res) {
+                $('#sim-artist-img-1').attr('src', res.image_url);
+            });
 
-//             $.ajax({
-//                 url: urlArray[2],
-//                 method: "GET",
-//             }).then(function(res) {
-//                 $('#sim-artist-img-2').attr('src', res.image_url);
-//             });
+            $.ajax({
+                url: urlArray[2],
+                method: "GET",
+            }).then(function(res) {
+                $('#sim-artist-img-2').attr('src', res.image_url);
+            });
             
-//         });
-//     }
-// });
+        });
+    }
+});
 
 
             // ------------- CODE THAT WE TRIED ----------------
@@ -124,8 +124,11 @@ $(document).on('keypress',function(e) {
 
             // Getting and pasting artist name into Jumbotron
             $("#artist-name").text(response.name);
+            $('#artist-name').addClass('animate__animated animate__fadeInRight');
             
-            // $('.jumbotron-fluid').attr('background-image', url(response.image_url));
+            var jumboImg = response.image_url;
+            console.log(jumboImg);
+            $('.jumbotron-image').attr('style', 'background-image:' + "url(" + jumboImg + ")");
 
              // if artist has a fb page, a clickable fb icon will appear in the social tab and redirects you in a new tab
             if (response.facebook_page_url === '') {
