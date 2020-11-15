93e2d5fa19426cd6c825aceeaa1bd6f0d49105c8
nartexyu <70301421+nartexyu@users.noreply.github.com>
2020-11-14 19:45:09 -0800

    changed localstorage name

--- homeScript.js
+++ homeScript.js
@@ -1,21 +1,18 @@
-
-
 $(document).on('keypress',function(e) {
     if(e.which == 13) {   
         var artist = encodeURIComponent($('.form-control').val().toLowerCase());
-    localStorage.setItem('userInput', artist)
-    if (artist!=="") {
-    window.location.href="./index.html"
-}
-
-}
-})
+        localStorage.setItem('lastSearch', artist);
+        if (artist!=="") {
+            window.location.href="index.html"
+        };
+    };   
+});
 
 
 $("#button-addon2").on("click", function() {
-   var artist = encodeURIComponent($('.form-control').val().toLowerCase());
-    localStorage.setItem('userInput', artist)
+    var artist = encodeURIComponent($('.form-control').val().toLowerCase());
+    localStorage.setItem('lastSearch', artist)
     if (artist!=="") {
-        window.location.href="./index.html"
-    }
+        window.location.href="index.html"
+    };
 });
